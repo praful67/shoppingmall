efe11289ef4d673492a330a3a7e6bb0b1fcdc029
Praful <2017UEE0067@iitjammu.ac.in>
2019-12-19 21:54:22 +0530

    Updates

---
+++ precache-manifest.635934099fdaec570f3f8d185fdab8e9.js
@@ -0,0 +1,306 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e838a0e775adff3c7512bbb10c4b605",
+    "url": "/shoppingmall/index.html"
+  },
+  {
+    "revision": "73b3ac963349fe558720",
+    "url": "/shoppingmall/static/css/0.a3c6ef96.chunk.css"
+  },
+  {
+    "revision": "2ba9a9562a4c5d7b1858",
+    "url": "/shoppingmall/static/css/1.2f1df56b.chunk.css"
+  },
+  {
+    "revision": "3b94babc7cbe7b9cd0be",
+    "url": "/shoppingmall/static/css/10.07100997.chunk.css"
+  },
+  {
+    "revision": "6672a87487af5440ada6",
+    "url": "/shoppingmall/static/css/11.62027c08.chunk.css"
+  },
+  {
+    "revision": "0f3de8849bc996ae8a34",
+    "url": "/shoppingmall/static/css/12.46790a42.chunk.css"
+  },
+  {
+    "revision": "2424359406df2f4270b8",
+    "url": "/shoppingmall/static/css/13.e2f9c463.chunk.css"
+  },
+  {
+    "revision": "b512466e3cb8d32a7fb8",
+    "url": "/shoppingmall/static/css/14.d7fcd0da.chunk.css"
+  },
+  {
+    "revision": "1c27db7417154913484d",
+    "url": "/shoppingmall/static/css/15.52d35ff4.chunk.css"
+  },
+  {
+    "revision": "536c2b6e2c369d278c19",
+    "url": "/shoppingmall/static/css/16.d175fe9e.chunk.css"
+  },
+  {
+    "revision": "ffae8d23b3b07ce7bad6",
+    "url": "/shoppingmall/static/css/2.dfcb72d0.chunk.css"
+  },
+  {
+    "revision": "eed9012bddae452674bc",
+    "url": "/shoppingmall/static/css/5.5125fc78.chunk.css"
+  },
+  {
+    "revision": "d0ac3b983b126069d588",
+    "url": "/shoppingmall/static/css/6.6d7d54d2.chunk.css"
+  },
+  {
+    "revision": "98b1772122cc890d7e31",
+    "url": "/shoppingmall/static/css/7.16db0f2c.chunk.css"
+  },
+  {
+    "revision": "b000228a9fb4b4f6dc5b",
+    "url": "/shoppingmall/static/css/8.3566dd60.chunk.css"
+  },
+  {
+    "revision": "aa610103e524a88a1968",
+    "url": "/shoppingmall/static/css/main.a0bd4be2.chunk.css"
+  },
+  {
+    "revision": "73b3ac963349fe558720",
+    "url": "/shoppingmall/static/js/0.90c70bb4.chunk.js"
+  },
+  {
+    "revision": "e0002910fc972ad8a92fb646ee516370",
+    "url": "/shoppingmall/static/js/0.90c70bb4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2ba9a9562a4c5d7b1858",
+    "url": "/shoppingmall/static/js/1.f448d8fa.chunk.js"
+  },
+  {
+    "revision": "3b94babc7cbe7b9cd0be",
+    "url": "/shoppingmall/static/js/10.cb6bd235.chunk.js"
+  },
+  {
+    "revision": "6672a87487af5440ada6",
+    "url": "/shoppingmall/static/js/11.cef144ee.chunk.js"
+  },
+  {
+    "revision": "0f3de8849bc996ae8a34",
+    "url": "/shoppingmall/static/js/12.a8c37959.chunk.js"
+  },
+  {
+    "revision": "2424359406df2f4270b8",
+    "url": "/shoppingmall/static/js/13.80774b64.chunk.js"
+  },
+  {
+    "revision": "b512466e3cb8d32a7fb8",
+    "url": "/shoppingmall/static/js/14.ae22d05f.chunk.js"
+  },
+  {
+    "revision": "1c27db7417154913484d",
+    "url": "/shoppingmall/static/js/15.cc54e540.chunk.js"
+  },
+  {
+    "revision": "536c2b6e2c369d278c19",
+    "url": "/shoppingmall/static/js/16.569c1feb.chunk.js"
+  },
+  {
+    "revision": "ffae8d23b3b07ce7bad6",
+    "url": "/shoppingmall/static/js/2.8fc6ada2.chunk.js"
+  },
+  {
+    "revision": "eed9012bddae452674bc",
+    "url": "/shoppingmall/static/js/5.26283f4d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/shoppingmall/static/js/5.26283f4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0ac3b983b126069d588",
+    "url": "/shoppingmall/static/js/6.cb1536c4.chunk.js"
+  },
+  {
+    "revision": "98b1772122cc890d7e31",
+    "url": "/shoppingmall/static/js/7.da50add1.chunk.js"
+  },
+  {
+    "revision": "b000228a9fb4b4f6dc5b",
+    "url": "/shoppingmall/static/js/8.6de8a139.chunk.js"
+  },
+  {
+    "revision": "1a4f42857c34fb3093aa",
+    "url": "/shoppingmall/static/js/9.b7647683.chunk.js"
+  },
+  {
+    "revision": "aa610103e524a88a1968",
+    "url": "/shoppingmall/static/js/main.f627403a.chunk.js"
+  },
+  {
+    "revision": "23ea198dddaf255a5c25",
+    "url": "/shoppingmall/static/js/runtime-main.0e3b4a72.js"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/shoppingmall/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/shoppingmall/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/shoppingmall/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/shoppingmall/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/shoppingmall/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/shoppingmall/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/shoppingmall/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/shoppingmall/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/shoppingmall/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/shoppingmall/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/shoppingmall/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/shoppingmall/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/shoppingmall/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/shoppingmall/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/shoppingmall/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/shoppingmall/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/shoppingmall/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/shoppingmall/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/shoppingmall/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/shoppingmall/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/shoppingmall/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/shoppingmall/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/shoppingmall/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/shoppingmall/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/shoppingmall/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/shoppingmall/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/shoppingmall/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/shoppingmall/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/shoppingmall/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/shoppingmall/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/shoppingmall/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/shoppingmall/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/shoppingmall/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/shoppingmall/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/shoppingmall/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/shoppingmall/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/shoppingmall/static/media/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/shoppingmall/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/shoppingmall/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/shoppingmall/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/shoppingmall/static/media/slick.f97e3bbf.svg"
+  }
+]);
